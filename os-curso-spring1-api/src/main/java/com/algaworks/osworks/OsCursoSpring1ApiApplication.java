44f71ea3f19a3d3aaca0acbfbf85ba1904555c08
94rodrigo <rodrigoc94@hotmail.com>
2020-04-29 22:22:24 -0300

    Commit #01

---
+++ os-curso-spring1-api/src/main/java/com/algaworks/osworks/OsCursoSpring1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsCursoSpring1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsCursoSpring1ApiApplication.class, args);
+	}
+
+}
---
+++ os-curso-spring1-api/src/test/java/com/algaworks/osworks/OsCursoSpring1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsCursoSpring1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
